40c1d50aec299371e6361946ca54cb836cafeef4
kamau-David <dayvieek@gmail.com>
2026-02-23 23:29:28 +0300

    joining paths

--- path-modules/index.js
+++ path-modules/index.js
@@ -4,4 +4,7 @@ console.log('Directory name', path.dirname(__filename));
 
 console.log('filename', path.basename(__filename));
 
-console.log('File extension', path.extname(__filename))
+console.log('File extension', path.extname(__filename));
+
+const joinPath = path.join('/users','documents','node','project');
+console.log('Joined path', joinPath)
