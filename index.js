07fe5d20208dec2c1bbac440e8be09ca87cecfdb
kamau-David <dayvieek@gmail.com>
2026-02-24 00:00:04 +0300

    initial'

--- index.js
+++ index.js
@@ -4,3 +4,19 @@ fs.writeFileSync('notes.txt', 'I love California')
 
 
 console.log('hello world')
+const path = require('path');
+
+console.log('Directory name', path.dirname(__filename));
+
+console.log('filename', path.basename(__filename));
+
+console.log('File extension', path.extname(__filename));
+
+const joinPath = path.join('/users','documents','node','project');
+console.log('Joined path', joinPath)
+
+const resolvePath = path.resolve('/users','documents','node','project');
+console.log('resolved path', resolvePath)
+
+const normalizePath = path.normalize('users','documents','node','project');
+console.log('Normalized path', normalizePath)
