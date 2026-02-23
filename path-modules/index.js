5f39e25f806c22a8a35a1252cafc067fda538163
kamau-David <dayvieek@gmail.com>
2026-02-23 23:16:21 +0300

    how to find an extension

--- path-modules/index.js
+++ path-modules/index.js
@@ -2,4 +2,6 @@ const path = require('path');
 
 console.log('Directory name', path.dirname(__filename));
 
-console.log('filename', path.basename(__filename))
+console.log('filename', path.basename(__filename));
+
+console.log('File extension', path.extname(__filename))
