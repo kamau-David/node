942fb5d795a5d4c60e8c099f84ab6b1f6987447c
kamau-David <dayvieek@gmail.com>
2026-02-24 12:05:01 +0300

    creating data folders

---
+++ file-system/index.js
@@ -0,0 +1,11 @@
+const fs = require("fs");
+const path = require("path");
+
+const dataFolder = path.join(__dirname, "data");
+
+if (!fs.existsSync(dataFolder)) {
+  fs.mkdirSync(dataFolder);
+  console.log("data folder created");
+} else {
+  console.log("file already exists");
+}
