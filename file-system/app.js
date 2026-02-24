b4d31935d2839e43223c2b9d0140cb9879d622e6
kamau-David <dayvieek@gmail.com>
2026-02-24 12:36:58 +0300

    new line

--- file-system/app.js
+++ file-system/app.js
@@ -4,6 +4,7 @@ const path = require("path");
 const dataFolder = path.join(__dirname, "data");
 
 if (!fs.existsSync(dataFolder)) {
+  //creating a directory
   fs.mkdirSync(dataFolder);
   console.log("data folder created");
 } else {
@@ -13,7 +14,10 @@ if (!fs.existsSync(dataFolder)) {
 const filePath = path.join(dataFolder, "example.txt");
 
 fs.writeFileSync(filePath, "Hello am David Kamau");
-console.log("file created");
+console.log("file created"); //creating a file
 
 const readFileContent = fs.readFileSync(filePath, "utf-8");
-console.log("File content : ", readFileContent);
+console.log("File content : ", readFileContent); //reading file content
+
+fs.appendFileSync(filePath, "\n Am 21 year old ");
+console.log("new line added");
