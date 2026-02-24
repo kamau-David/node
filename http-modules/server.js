93b00eb2d1a00e465d090533f0dff8d13e2d7722
kamau-David <dayvieek@gmail.com>
2026-02-24 13:28:00 +0300

    creating a basic server

---
+++ http-modules/server.js
@@ -0,0 +1,13 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  console.log(req, "req");
+
+  res.writeHead(200, { "content-type": "text/plain" });
+  res.end("Hello there");
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
