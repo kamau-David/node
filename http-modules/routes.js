da8de5a8a40a25ecccc7dc59fa5e1210258bdec8
kamau-David <dayvieek@gmail.com>
2026-02-24 13:38:16 +0300

    routing

---
+++ http-modules/routes.js
@@ -0,0 +1,21 @@
+const http = require("http");
+
+const server = http.createServer((req, res) => {
+  const url = req.url;
+
+  if (url == "/") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("Home page");
+  } else if (url == "/about") {
+    res.writeHead(200, { "content-type": "text/plain" });
+    res.end("About this page");
+  } else {
+    res.writeHead(404, { "content-type": "text/plain" });
+    res.end("page not found");
+  }
+});
+
+const port = 3000;
+server.listen(port, () => {
+  console.log(`server listening on port ${port}`);
+});
