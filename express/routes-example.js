938c2d14f3f683a50bfc791c4e09f053de118ce1
kamau-David <dayvieek@gmail.com>
2026-02-25 23:26:37 +0300

    getting a single product

--- express/routes-example.js
+++ express/routes-example.js
@@ -26,6 +26,33 @@ app.get("/products", (req, res) => {
   res.json(products);
 });
 
+app.get("/products/:id", (req, res) => {
+  const productId = req.params.id;
+  const products = [
+    {
+      id: "1",
+      label: "product 1",
+    },
+
+    {
+      id: "2",
+      label: "product 2",
+    },
+
+    {
+      id: "3",
+      label: "product 3",
+    },
+  ];
+  const getSingleProduct = products.find((product) => product.id === productId);
+
+  if (getSingleProduct) {
+    res.json(getSingleProduct);
+  } else {
+    res.status(404).send("product not found please try another id");
+  }
+});
+
 const port = 3000;
 app.listen(port, () => {
   console.log(`server is running on port ${port}`);
