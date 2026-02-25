f14c727eb3fc9ed9fb5cae5b270dcf83b0fb12e6
kamau-David <dayvieek@gmail.com>
2026-02-25 23:04:05 +0300

    initial commit

---
+++ express/routes-example.js
@@ -0,0 +1,32 @@
+const express = require("express");
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Welcome to our home page");
+});
+
+app.get("/products", (req, res) => {
+  const products = [
+    {
+      id: "1",
+      label: "product 1",
+    },
+
+    {
+      id: "2",
+      label: "product 2",
+    },
+
+    {
+      id: "3",
+      label: "product 3",
+    },
+  ];
+  res.json(products);
+});
+
+const port = 3000;
+app.listen(port, () => {
+  console.log(`server is running on port ${port}`);
+});
